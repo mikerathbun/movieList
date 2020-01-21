810820c9b3b1d83fc43f0e8685fb98df81b480b6
Mike Rathbun <mikerathbun@me.com>
2020-01-21 11:53:44 -0600

    Started components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './containers/List'
+
 
 const App = () => {
-    return <h1>movieList</h1>;
+    return <List />;
 
 };
 ReactDOM.render(<App />, document.getElementById('root'));
